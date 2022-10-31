e5bfe30f65b69ac9a7c320afd744f1038b06c048
jstart <11465401+jstart@user.noreply.gitee.com>
2022-10-31 11:54:50 +0800

    前端部分-Vue脚手架项目

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,141 +2,168 @@
 .layout-header {
   background: #1684b0;
 }
+
 .layout-header h1 {
   color: #fff;
   text-align: left;
   line-height: 60px;
 }
-i{
-  color: white !important;
+
+.layout-body {
+  position: absolute;
+  top: 60px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+}
+
+.layout-aside {
+  background: #222c32;
+}
+
+.layout-aside i {
+  color: #fff !important;
+}
+
+.layout-main {
+}
+
+.el-menu-item.is-active {
+  background: #2d3c4d !important;
 }
 </style>
-
 <template>
   <div>
     <!-- 最外层容器 -->
     <el-container>
-      <!-- 上半部分: 顶栏 -->
+      <!-- 上半部分：顶栏 -->
       <el-header class="layout-header">
         <h1>酷鲨商城运营管理平台</h1>
       </el-header>
-      <!-- 下半部分: 容器 -->
-      <el-container>
+      <!-- 下半部分：容器 -->
+      <el-container class="layout-body">
         <!-- 下半部分的左侧边栏 -->
-        <el-aside width="260px">
+        <el-aside class="layout-aside">
           <el-menu
-              style="height: 1000px"
-              default-active="1"
+              router
+              :default-active="$router.currentRoute.path"
               class="el-menu-vertical-demo"
-              background-color="#2c3e50"
+              background-color="#222c32"
               text-color="#fff"
-              active-text-color="#ffd04b">
-            <el-menu-item index="1">
+              active-text-color="#fff">
+            <!-- 首页 -->
+            <el-menu-item index="/sys-admin">
               <i class="el-icon-s-home"></i>
-              <span slot="title">首页</span>
+              <span>首页</span>
             </el-menu-item>
+            <!-- 临时页面 -->
             <el-submenu index="2">
               <template slot="title">
-                <i class="el-icon-setting"></i>
+                <i class="el-icon-s-tools"></i>
                 <span>临时页面</span>
               </template>
-              <el-menu-item index="1-1">
-                <i class="el-icon-setting"></i>
-                <span slot="title">添加管理员</span>
+              <el-menu-item index="/sys-admin/temp/admin/add-new">
+                <i class="el-icon-circle-plus"></i>
+                <span>添加管理员</span>
               </el-menu-item>
-              <el-menu-item index="1-2">
-                <i class="el-icon-setting"></i>
-                <span slot="title">管理员列表</span>
+              <el-menu-item index="/sys-admin/temp/admin/list">
+                <i class="el-icon-s-operation"></i>
+                <span>管理员列表</span>
               </el-menu-item>
-              <el-menu-item index="1-3">
-                <i class="el-icon-setting"></i>
-                <span slot="title">添加品牌</span>
+              <el-menu-item index="/sys-admin/temp/brand/add-new">
+                <i class="el-icon-circle-plus"></i>
+                <span>添加品牌</span>
               </el-menu-item>
-              <el-menu-item index="1-4">
-                <i class="el-icon-setting"></i>
-                <span slot="title">品牌列表</span>
+              <el-menu-item index="/sys-admin/temp/brand/list">
+                <i class="el-icon-s-operation"></i>
+                <span>品牌列表</span>
               </el-menu-item>
-              <el-menu-item index="1-5">
-                <i class="el-icon-setting"></i>
-                <span slot="title">添加类别</span>
+              <el-menu-item index="/sys-admin/temp/category/add-new">
+                <i class="el-icon-circle-plus"></i>
+                <span>添加类别</span>
               </el-menu-item>
-              <el-menu-item index="1-6">
-                <i class="el-icon-setting"></i>
-                <span slot="title">类别列表</span>
+              <el-menu-item index="/sys-admin/temp/category/list">
+                <i class="el-icon-s-operation"></i>
+                <span>类别列表</span>
               </el-menu-item>
-              <el-menu-item index="1-7">
-                <i class="el-icon-setting"></i>
-                <span slot="title">添加相册</span>
+              <el-menu-item index="/sys-admin/temp/album/add-new">
+                <i class="el-icon-circle-plus"></i>
+                <span>添加相册</span>
               </el-menu-item>
-              <el-menu-item index="1-8">
-                <i class="el-icon-setting"></i>
-                <span slot="title">相册列表</span>
+              <el-menu-item index="/sys-admin/temp/album/list">
+                <i class="el-icon-s-operation"></i>
+                <span>相册列表</span>
               </el-menu-item>
-              <el-menu-item index="1-9">
-                <i class="el-icon-setting"></i>
-                <span slot="title">添加属性模板</span>
+              <el-menu-item index="/sys-admin/temp/attributeTemplate/add-new">
+                <i class="el-icon-circle-plus"></i>
+                <span>添加属性模板</span>
               </el-menu-item>
-              <el-menu-item index="1-10">
-                <i class="el-icon-setting"></i>
-                <span slot="title">属性模板列表</span>
+              <el-menu-item index="/sys-admin/temp/attributeTemplate/list">
+                <i class="el-icon-s-operation"></i>
+                <span>属性模板列表</span>
               </el-menu-item>
-              <el-menu-item index="1-11">
-                <i class="el-icon-setting"></i>
-                <span slot="title">添加属性</span>
+              <el-menu-item index="/sys-admin/temp/attribute/add-new">
+                <i class="el-icon-circle-plus"></i>
+                <span>添加属性</span>
               </el-menu-item>
-              <el-menu-item index="1-12">
-                <i class="el-icon-setting"></i>
-                <span slot="title">属性列表</span>
+              <el-menu-item index="/sys-admin/temp/attribute/list">
+                <i class="el-icon-s-operation"></i>
+                <span>属性列表</span>
               </el-menu-item>
             </el-submenu>
+            <!-- 商品管理 -->
             <el-submenu index="3">
               <template slot="title">
                 <i class="el-icon-s-goods"></i>
                 <span>商品管理</span>
               </template>
-              <el-menu-item index="2-1">
-                <i class="el-icon-menu"></i>
+              <el-menu-item index="3-1">
+                <i class="el-icon-s-grid"></i>
                 <span slot="title">SPU台账</span>
               </el-menu-item>
-              <el-menu-item index="2-2">
-                <i class="el-icon-menu"></i>
+              <el-menu-item index="3-2">
+                <i class="el-icon-circle-plus"></i>
                 <span slot="title">新增SPU</span>
               </el-menu-item>
-              <el-menu-item index="2-3">
-                <i class="el-icon-menu"></i>
+              <el-menu-item index="3-3">
+                <i class="el-icon-picture"></i>
                 <span slot="title">相册管理</span>
               </el-menu-item>
-              <el-menu-item index="2-4">
-                <i class="el-icon-menu"></i>
+              <el-menu-item index="3-4">
+                <i class="el-icon-s-unfold"></i>
                 <span slot="title">商品类别</span>
               </el-menu-item>
-              <el-menu-item index="2-5">
-                <i class="el-icon-menu"></i>
+              <el-menu-item index="3-5">
+                <i class="el-icon-s-flag"></i>
                 <span slot="title">属性模板</span>
               </el-menu-item>
-              <el-menu-item index="2-6">
-                <i class="el-icon-menu"></i>
+              <el-menu-item index="3-6">
+                <i class="el-icon-s-data"></i>
                 <span slot="title">品牌管理</span>
               </el-menu-item>
             </el-submenu>
+            <!-- 订单管理 -->
             <el-submenu index="4">
               <template slot="title">
                 <i class="el-icon-s-order"></i>
                 <span>订单管理</span>
               </template>
             </el-submenu>
+            <!-- 营销管理 -->
             <el-submenu index="5">
               <template slot="title">
                 <i class="el-icon-s-ticket"></i>
                 <span>营销管理</span>
               </template>
             </el-submenu>
+            <!-- 商家管理 -->
             <el-submenu index="6">
               <template slot="title">
                 <i class="el-icon-s-shop"></i>
                 <span>商家管理</span>
               </template>
             </el-submenu>
+            <!-- 权限管理 -->
             <el-submenu index="7">
               <template slot="title">
                 <i class="el-icon-s-check"></i>
@@ -146,7 +173,9 @@ i{
           </el-menu>
         </el-aside>
         <!-- 下半部分的右侧主体部分 -->
-        <el-main></el-main>
+        <el-main class="layout-main">
+          <router-view/>
+        </el-main>
       </el-container>
     </el-container>
   </div>
--- src/views/sys-admin/temp/AlbumAddNewView.vue
+++ src/views/sys-admin/temp/AlbumAddNewView.vue
@@ -4,9 +4,63 @@
       <el-breadcrumb-item :to="{ path: '/' }">后台管理</el-breadcrumb-item>
       <el-breadcrumb-item>添加相册</el-breadcrumb-item>
     </el-breadcrumb>
-
     <el-divider></el-divider>
 
-    <el-empty description="当前页面正在开发中,尽请期待..."></el-empty>
+    <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+      <el-form-item label="名称" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item label="简介" prop="description">
+        <el-input v-model="ruleForm.description"></el-input>
+      </el-form-item>
+      <el-form-item label="排序序号" prop="sort">
+        <el-input v-model="ruleForm.sort"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm')">登录</el-button>
+        <el-button @click="resetForm('ruleForm')">重置</el-button>
+      </el-form-item>
+    </el-form>
   </div>
 </template>
+<script>
+export default {
+  data() {
+    return {
+      ruleForm: {
+        name: '',
+        description: '',
+        sort: ''
+      },
+      rules: {
+        name: [
+          {required: true, message: '请输入相册名称', trigger: 'blur'},
+          {min: 4, max: 15, message: '长度在 4 到 15 个字符', trigger: 'blur'}
+        ],
+        description: [
+          {required: true, message: '请输入相册简介', trigger: 'blur'},
+          {min: 4, max: 35, message: '长度在 4 到 35 个字符', trigger: 'blur'}
+        ],
+        sort: [
+          {required: true, message: '请输入排序序号', trigger: 'blur'}
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!');
+        } else {
+          console.log('error submit!!');
+          return false;
+        }
+      });
+    },
+    resetForm(formName) {
+      this.$refs[formName].resetFields();
+    }
+  }
+}
+</script>
