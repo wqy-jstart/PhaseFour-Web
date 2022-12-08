0d72f039596d1e5a9b063cbd4806eb2e6ccb3bad
Devotion <11465401+jstart@user.noreply.gitee.com>
2022-12-08 17:19:29 +0800

    ....

--- src/router/index.js
+++ src/router/index.js
@@ -6,92 +6,92 @@ Vue.use(VueRouter)
 
 //★配置路由(新的页面,必须要有一个路由配置才可通过网址进行访问)
 const routes = [
-  {
-    path: '/',
-    component: HomeView,
-    redirect:'/sys-admin',
-    children:[ //嵌套路由,在HomeView里的router里
-      {
-        path: '/sys-admin',
-        component: () => import( '../views/sys-admin/SystemAdminIndex.vue')
-      },
-      {
-        path: '/sys-admin/temp/admin/add-new',
-        component: () => import( '../views/sys-admin/temp/AdminAddNewView.vue')
-      },
-      {
-        path: '/sys-admin/temp/admin/list',
-        component: () => import( '../views/sys-admin/temp/AdminListView.vue')
-      },
-      {
-        path: '/sys-admin/temp/album/add-new',
-        component: () => import( '../views/sys-admin/temp/AlbumAddNewView.vue')
-      },
-      {
-        path: '/sys-admin/temp/album/list',
-        component: () => import( '../views/sys-admin/temp/AlbumListView.vue')
-      },
-      {
-        path: '/sys-admin/temp/attribute/add-new',
-        component: () => import( '../views/sys-admin/temp/AttributeAddNewView.vue')
-      },
-      {
-        path: '/sys-admin/temp/attribute/list',
-        component: () => import( '../views/sys-admin/temp/AttributeListView.vue')
-      },
-      {
-        path: '/sys-admin/temp/attributeTemplate/add-new',
-        component: () => import( '../views/sys-admin/temp/AttributeTemplateAddNewView.vue')
-      },
-      {
-        path: '/sys-admin/temp/attributeTemplate/list',
-        component: () => import( '../views/sys-admin/temp/AttributeTemplateListView.vue')
-      },
-      {
-        path: '/sys-admin/temp/brand/add-new',
-        component: () => import( '../views/sys-admin/temp/BrandAddNewView.vue')
-      },
-      {
-        path: '/sys-admin/temp/brand/list',
-        component: () => import( '../views/sys-admin/temp/BrandListView.vue')
-      },
-      {
-        path: '/sys-admin/temp/category/add-new',
-        component: () => import( '../views/sys-admin/temp/CategoryAddNewView.vue')
-      },
-      {
-        path: '/sys-admin/temp/category/list',
-        component: () => import( '../views/sys-admin/temp/CategoryListView.vue')
-      },
-        // --------------------------------------------------------------------
-      {
-        path: '/sys-admin/product/spu-add-new',
-        component: () => import( '../views/sys-admin/product/SpuAddNewStep1View.vue')
-      },
-      {
-        path: '/sys-admin/product/spu-add-new2',
-        component: () => import('../views/sys-admin/product/SpuAddNewStep2View.vue')
-      },
-      {
-        path: '/sys-admin/product/spu-add-new3',
-        component: () => import('../views/sys-admin/product/SpuAddNewStep3View.vue')
-      },
-      {
-        path: '/sys-admin/product/spu-add-new4',
-        component: () => import('../views/sys-admin/product/SpuAddNewStep4View.vue')
-      }
-    ]
-  },
-  {
-    path: '/login',
-    component: () => import( '../views/LoginView.vue')
-  },
+    {
+        path: '/',
+        component: HomeView,
+        redirect: '/sys-admin',
+        children: [ //嵌套路由,在HomeView里的router里
+            {
+                path: '/sys-admin',
+                component: () => import( '../views/sys-admin/SystemAdminIndex.vue')
+            },
+            {
+                path: '/sys-admin/temp/admin/add-new',
+                component: () => import( '../views/sys-admin/temp/AdminAddNewView.vue')
+            },
+            {
+                path: '/sys-admin/temp/admin/list',
+                component: () => import( '../views/sys-admin/temp/AdminListView.vue')
+            },
+            {
+                path: '/sys-admin/temp/album/add-new',
+                component: () => import( '../views/sys-admin/temp/AlbumAddNewView.vue')
+            },
+            {
+                path: '/sys-admin/temp/album/list',
+                component: () => import( '../views/sys-admin/temp/AlbumListView.vue')
+            },
+            {
+                path: '/sys-admin/temp/attribute/add-new',
+                component: () => import( '../views/sys-admin/temp/AttributeAddNewView.vue')
+            },
+            {
+                path: '/sys-admin/temp/attribute/list',
+                component: () => import( '../views/sys-admin/temp/AttributeListView.vue')
+            },
+            {
+                path: '/sys-admin/temp/attributeTemplate/add-new',
+                component: () => import( '../views/sys-admin/temp/AttributeTemplateAddNewView.vue')
+            },
+            {
+                path: '/sys-admin/temp/attributeTemplate/list',
+                component: () => import( '../views/sys-admin/temp/AttributeTemplateListView.vue')
+            },
+            {
+                path: '/sys-admin/temp/brand/add-new',
+                component: () => import( '../views/sys-admin/temp/BrandAddNewView.vue')
+            },
+            {
+                path: '/sys-admin/temp/brand/list',
+                component: () => import( '../views/sys-admin/temp/BrandListView.vue')
+            },
+            {
+                path: '/sys-admin/temp/category/add-new',
+                component: () => import( '../views/sys-admin/temp/CategoryAddNewView.vue')
+            },
+            {
+                path: '/sys-admin/temp/category/list',
+                component: () => import( '../views/sys-admin/temp/CategoryListView.vue')
+            },
+            // --------------------------------------------------------------------
+            {
+                path: '/sys-admin/product/spu-add-new',
+                component: () => import( '../views/sys-admin/product/SpuAddNewStep1View.vue')
+            },
+            {
+                path: '/sys-admin/product/spu-add-new2',
+                component: () => import('../views/sys-admin/product/SpuAddNewStep2View.vue')
+            },
+            {
+                path: '/sys-admin/product/spu-add-new3',
+                component: () => import('../views/sys-admin/product/SpuAddNewStep3View.vue')
+            },
+            {
+                path: '/sys-admin/product/spu-add-new4',
+                component: () => import('../views/sys-admin/product/SpuAddNewStep4View.vue')
+            }
+        ]
+    },
+    {
+        path: '/login',
+        component: () => import( '../views/LoginView.vue')
+    },
 ]
 
 const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
 })
 
 export default router
